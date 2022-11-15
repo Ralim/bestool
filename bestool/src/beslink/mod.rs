a1a33052ae5ebee9bdf2ffbb7022d89dd9c35eb9
Ben V. Brown <Ralim@Ralimtek.com>
2022-11-15 19:37:29 +1100

    expose tools

--- bestool/src/beslink/mod.rs
+++ bestool/src/beslink/mod.rs
@@ -4,5 +4,10 @@ mod packet;
 mod sync;
 
 pub const BES_PROGRAMMING_BAUDRATE: u32 = 921600;
+pub const BES_SYNC: u8 = 0xBE;
 pub use errors::BESLinkError;
+pub use message::BesMessage;
+pub use message::MessageTypes;
+pub use packet::read_packet;
+pub use packet::send_packet;
 pub use sync::sync;
