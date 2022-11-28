7d4c7b3e95527283ae456753b984b6bd33b6fcb6
Ben V. Brown <Ralim@Ralimtek.com>
2022-11-29 07:19:21 +1100

    Better read logging

--- bestool/src/beslink/read_flash.rs
+++ bestool/src/beslink/read_flash.rs
@@ -4,7 +4,7 @@ use crate::beslink::{
 };
 use serialport::SerialPort;
 use std::time::Duration;
-use tracing::info;
+use tracing::{info, warn};
 
 pub fn read_flash_data(
     serial_port: &mut Box<dyn SerialPort>,
@@ -12,16 +12,27 @@ pub fn read_flash_data(
     length: usize,
 ) -> Result<Vec<u8>, BESLinkError> {
     let mut result = vec![];
+    let mut tries = 0;
     while result.len() < length {
-        let chunk = read_flash_chunk(serial_port, address + result.len())?;
-        result.extend_from_slice(&chunk);
-        std::thread::sleep(Duration::from_millis(10));
-        info!(
-            "Read {} bytes out of {}  ({}%) from flash",
-            result.len(),
-            length,
-            result.len() * 100 / length
-        );
+        match read_flash_chunk(serial_port, address + result.len()) {
+            Ok(chunk) => {
+                result.extend_from_slice(&chunk);
+                std::thread::sleep(Duration::from_millis(10)); // Try to yield to let watch dog reset
+                info!(
+                    "Read {} bytes out of {}  ({}%) from flash",
+                    result.len(),
+                    length,
+                    result.len() * 100 / length
+                );
+            }
+            Err(e) => {
+                warn!("Error {:?}", e);
+                tries += 1;
+                if tries > 10 {
+                    return Err(e);
+                }
+            }
+        }
     }
     result.resize(length, 0xFF);
     return Ok(result);
@@ -39,7 +50,7 @@ fn read_flash_chunk(
         payload: vec![0x05, 0x08], // No idea what these two mean yet
         checksum: 0xF6,
     };
-    let chunk_size = (FLASH_BUFFER_SIZE / 2);
+    let chunk_size = FLASH_BUFFER_SIZE / 2;
     cfg_data_1.payload.extend((address as u32).to_le_bytes());
     cfg_data_1.payload.extend((chunk_size as u32).to_le_bytes());
     cfg_data_1.set_checksum();
