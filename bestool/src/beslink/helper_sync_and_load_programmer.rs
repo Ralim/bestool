d83c436539a7992dbab1752806e5aab3c5908265
Ben V. Brown <Ralim@Ralimtek.com>
2022-12-27 22:18:57 +1100

    Assert on sync response message

--- bestool/src/beslink/helper_sync_and_load_programmer.rs
+++ bestool/src/beslink/helper_sync_and_load_programmer.rs
@@ -3,7 +3,7 @@ use crate::beslink::{
     start_programmer_runtime_binary_blob, sync, BESLinkError, BesMessage, MessageTypes, BES_SYNC,
 };
 use serialport::SerialPort;
-use tracing::info;
+use tracing::{info, warn};
 
 pub fn helper_sync_and_load_programmer(
     serial_port: &mut Box<dyn SerialPort>,
@@ -21,17 +21,27 @@ pub fn helper_sync_and_load_programmer(
 fn sync_with_bootloader(serial_port: &mut Box<dyn SerialPort>) -> Result<(), BESLinkError> {
     // Gain sync
     info!("Syncing into bootloader");
-    let _ = sync(serial_port, MessageTypes::Sync)?;
-    info!("Saw boot sync, sending ack");
-    // Send message to stay in bootloader
-    let msg = BesMessage {
-        sync: BES_SYNC,
-        type1: MessageTypes::Sync,
-        payload: vec![0x00, 0x01, 0x01],
-        checksum: 0xEF,
-    };
-    return match send_message(serial_port, msg) {
-        Ok(_) => Ok(()),
-        Err(e) => Err(BESLinkError::from(e)),
-    };
+    let sync_message = sync(serial_port, MessageTypes::Sync)?;
+    info!("Received sync advertisement {:X?}", sync_message.to_vec());
+    loop {
+        info!("Saw boot sync, sending ack");
+        // Send message to stay in bootloader
+        let msg = BesMessage {
+            sync: BES_SYNC,
+            type1: MessageTypes::Sync,
+            payload: vec![0x00, 0x01, 0x01],
+            checksum: 0xEF,
+        };
+        match send_message(serial_port, msg) {
+            Ok(_) => {}
+            Err(e) => return Err(BESLinkError::from(e)),
+        };
+        info!("Sent sync message");
+        let response = sync(serial_port, MessageTypes::Sync)?;
+        if response.payload[0] == 0x00 {
+            return Ok(());
+        } else {
+            warn!("Received bad sync response {:X?}", response.to_vec());
+        }
+    }
 }
