1ad551f754638d0b3f2ee4b44920c0d92ae23da4
Ben V. Brown <Ralim@Ralimtek.com>
2022-12-27 22:18:33 +1100

    Extra error for bad response

--- bestool/src/beslink/errors.rs
+++ bestool/src/beslink/errors.rs
@@ -6,8 +6,14 @@ pub enum BESLinkError {
     IOError(#[from] std::io::Error),
     #[error("SerialPortError")]
     SerialPortError(#[from] serialport::Error),
-    #[error("BadChecksumError Bad checksum; got {got:?} wanted {wanted:?} : {failed_packet:?}")]
+    #[error("BadChecksumError Bad checksum; got {got:?} wanted {wanted:?} : {failed_packet:X?}")]
     BadChecksumError {
+        failed_packet: Vec<u8>,
+        got: u8,
+        wanted: u8,
+    },
+    #[error("BadResponseCode Bad result; got {got:?} wanted {wanted:?} : {failed_packet:X?}")]
+    BadResponseCode {
         failed_packet: Vec<u8>,
         got: u8,
         wanted: u8,
